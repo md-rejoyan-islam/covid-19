7134523f0890915308517765bee50c99b3402692
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-11-08 01:48:41 +0600

    metadata added

---
+++ app/contagion/layout.tsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "Kvirus-19 | Contagion",
+  description:
+    "Kvirus-19 is a website that provides information about COVID-19.",
+  keywords: ["covid-19", "coronavirus", "pandemic"],
+  openGraph: {
+    title: "Kvirus-19 | Contagion",
+    description:
+      "Kvirus-19 is a website that provides information about COVID-19.",
+    keywords: ["covid-19", "coronavirus", "pandemic"],
+    type: "website",
+  },
+};
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}
